0b6cc6e551e59443eaaad98ea3c685692ab874b2
Koruto <flyingshadow03@gmail.com>
2025-02-03 01:30:40 +0530

    updated color scheme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeng/themes';
 import Aura from '@primeng/themes/aura';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{pink.50}',
+      100: '{pink.100}',
+      200: '{pink.200}',
+      300: '{pink.300}',
+      400: '{pink.400}',
+      500: '{pink.500}',
+      600: '{pink.600}',
+      700: '{pink.700}',
+      800: '{pink.800}',
+      900: '{pink.900}',
+      950: '{pink.950}',
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,7 +29,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
           darkModeSelector: '.my-app-light',
         },
